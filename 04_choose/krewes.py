3e9cba920e04c1a6c14d4d195870ab41ee5567e2
Vivian Teo <vivianteo950@gmail.com>
2022-09-22 13:12:39 -0400

    made the heading

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,4 +1,10 @@
-""""""
+"""
+Chicken Fries (Mahir Riki, Vivian Teo, Gabriel Thompson)
+SoftDev
+K04 -- python file that randomly generates a devopler
+2022-09-22
+time spent: 0.3 hours
+"""
 krewes = {2:["a2","b2","c2"], 7:["a7","b7","c7"], 8:["a8","b8","c8"]}
 
 import random
