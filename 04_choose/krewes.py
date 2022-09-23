55244b849dbc3331754cdf6bc3317ddd2abb6307
Vivian Teo <vivianteo950@gmail.com>
2022-09-23 12:53:43 -0400

    changed hours spent

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -3,7 +3,7 @@ Chicken Fries (Mahir Riki, Vivian Teo, Gabriel Thompson)
 SoftDev
 K04 -- python file that randomly generates a devopler
 2022-09-22
-time spent: 0.3 hours
+time spent: 0.5 hours
 """
 import dictionary
 import random
