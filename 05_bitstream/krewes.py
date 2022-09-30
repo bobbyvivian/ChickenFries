fc6a569727df81df2d1bb3bc3c902245a02f29ad
Vivian Teo <vivianteo950@gmail.com>
2022-09-30 12:43:17 -0400

    small edits

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,40 +1,73 @@
+# Vivian Teo, Emily Ortiz
+# SoftDev
+# 
+# 2022-__-__
+# time spent:
+
+# DISCO:
+# 
+# QCC:
+
 import random
 
-# krewes = {2:{"emily":"rain", "devoA":"duckyA"},
-#           7:{"vivian":"froggy", "devoB":"duckyB"},
-#           8:{"devo":"ducky", "devoC":"duckyC", "devoD":"duckyD"}
-#           }
+# what krewes should end up as
+# note: the periods are strings, not integers
+krewes0 = {'2':{"devoA":"duckyA", "devoB":"duckyB", "devo":"ducky"},
+          '7':{"vivian":"froggy", "emily":"rain"},
+          '8':{"devoC":"duckyC", "devoD":"duckyD"}
+          }
 
 # opening krewes.txt file
-f = open("krewes.txt", "r")
+file = open("krewes.txt", "r")
 # content has krewes.txt as a string
-content = f.read()
+content = file.read()
 
 # tuples is formatted as ["pd$$$devo$$$ducky", ...]
 tuples = content.split("@@@")
+# print("tuples:")
+# print(tuples)
+# print()
+
 # empty dictionary
 krewes = {}
 
-for person in tuples:
-    # splitting elements tuples into a lists of [pd, devo, ducky]
-    temp = person.split("$$$")
+for trio in tuples:
+    # splitting each element/trio in tuples into a list of [pd, devo, ducky]
+    temp = trio.split("$$$")
+    #print(temp)
     
     pd = temp[0]
     devo = temp[1]
     ducky = temp[2]
     
     if not (pd in krewes): # if there is no dictionary for that period yet
+        # add an empty dictionary for that period
         krewes[pd] = {}
     # adds devo as key, and ducky as value
     krewes[pd][devo] = ducky
 
+# print("krewes dictionary after being populated:")
 # print(krewes)
+# print()
 
 def random_dev(krewes):
+    # pick a random key/pd from a list of keys in krewes
     key = random.choice(list(dict.keys(krewes)))
-    nameDict = krewes[key]
-    devo = random.choice(list(dict.keys(nameDict)))
-    ducky = nameDict[devo]
-    print(str(key)+", "+devo+", "+ducky+", ")
-    
-random_dev(krewes)
+    # access dictionary for that key/pd
+    pd_dict = krewes[key]
+    # pick a random key/devo from a list of keys in that pd
+    devo = random.choice(list(dict.keys(pd_dict)))
+    # ducky is value for devo key
+    ducky = pd_dict[devo]
+    print(key+", "+devo+", "+ducky)
+
+# print("random_dev test:")
+# random_dev(krewes)
+
+print("Pick 10 random devos:")
+for i in range(10):
+    random_dev(krewes)
+
+# print()
+# print("Was krewes populated properly?")
+# print(krewes0 == krewes)
