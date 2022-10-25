0908f8ee4c6f3a3bab828b326e421bbf5297688b
Vivian Teo <vteo30@stuy.edu>
2022-10-25 19:58:19 -0400

    figured out why we couldn't add the values and made a second table for courses, works

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,5 +1,5 @@
 #Clyde "Thluffy" Sinclair
-#SoftDev  
+#SoftDev
 #skeleton/stub :: SQLITE3 BASICS
 #Oct 2022
 
@@ -10,34 +10,41 @@ DB_FILE="students.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-# c.execute("CREATE TABLE students(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)")
 
+# students table
+db.execute("DROP TABLE if exists students")
+c.execute("CREATE TABLE students(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)")
 
 with open('students.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    # for row in reader:
-    #     c.execute("INSERT INTO students VALUES (" + row['name'] + ", " + row['age'] + "," + row['id'] + ")")
-    # c.execute("SELECT * FROM students")
+    for row in reader:
+        c.execute("INSERT INTO students VALUES ('" + row['name'] + "', " + row['age'] + "," + row['id'] + ")")
 
-value = c.execute("SELECT * FROM students")
-value2 = value.fetchone()
-value2 = value.fetchone()
+students_table = c.execute("SELECT * FROM students")
+# print(students_table.fetchall())
 
-print(value2)
+# courses table
+DB_FILE="courses.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+db.execute("DROP TABLE if exists courses")
+c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)")
+
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute("INSERT INTO courses VALUES ('" + row['code'] + "', " + row['mark'] + "," + row['id'] + ")")
+
+courses_table = c.execute("SELECT * FROM courses")
+print(courses_table.fetchall())
 
 
-#==========================================================
-
-
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+# command = ""          # test SQL stmt in sqlite3 shell, save as string
+# c.execute(command)    # run SQL statement
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
