606e9468ea83da6f9288219b5f5afbd1fd49a93b
Vivian Teo <vteo30@stuy.edu>
2023-04-24 18:17:54 -0400

    figured out how to make a circle, changed color, notes updates

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -30,33 +30,39 @@ var drawRect = function(e) {
     var mouseX = e.clientX //gets X-coor of mouse when event is fired
     var mouseY = e.clientY //gets Y-coor of mouse when event is fired
     console.log("mouseclick registered at ", mouseX, mouseY)
-    ctx.fillRect(mouseX, mouseY, 10, 10)
+    ctx.fillStyle = "orange";
+    ctx.fillRect(mouseX, mouseY, 50, 60)
 }
 
 var drawCircle = function(e) {
     var mouseX = e.clientX //gets X-coor of mouse when event is fired
     var mouseY = e.clientY //gets Y-coor of mouse when event is fired
-    console.log("mouseclick registered at ", mouseX, mouseY);
-    ctx.fill(mouseX, mouseY, 10, 10)
+    console.log("mouseclick registered at ", mouseX, mouseY)
+    centerX = c.width/2;
+    centerY = c.width/2;
+    ctx.beginPath();
+    ctx.arc(mouseX,mouseY,20,0,2*Math.PI,false);
+    ctx.fillStyle = "orange";
+    ctx.fill();
+    ctx.stroke();    
 }
 
 var draw = function(e) {
-    // toggleMode;
-    // if (mode==="rect") {
-    //     drawRect;
-    // }
-    // else {
-    //     drawRect;
-    // }
-    drawRect
+    if (mode==="rect") {
+        console.log("rectangling...");    
+        drawRect(e);
+    }
+    else {
+        console.log("circling...");
+        drawCircle(e);
+    }
 }
 
 //var wipeCanvas = function() {
 var wipeCanvas = () => {
-    var canvas = getElementById("slate");
-    clearRect(canvas);
+    ctx.clearRect(0,0,c.width,c.height);
 }    
 
 c.addEventListener("click", draw) //passes the mouse event as parameter for the function
-
-makeButton.addEventListener("click", wipeCanvas())
+makeButton.addEventListener("click", toggleMode)
+wipeButton.addEventListener("click", wipeCanvas) //wipe canvas clean
